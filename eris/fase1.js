cc8daef99af7a7635dc0143ce3420bca1b9b4695
Luiza Biassi <luiza.biassi@gmail.com>
2021-04-16 21:15:11 -0300

    modulo 1

---
+++ eris/fase1.js
@@ -0,0 +1,32 @@
+function escolha(){
+    
+    var opcao = prompt("Faça sua escolha:\n1.\n2.");
+
+    while (opcao != 1 && opcao != 2){
+        alert("Essa opção não existe.");
+        opcao = prompt("Faça sua escolha:\n1.\n2.");
+    }
+
+    if (opcao == 1){
+        document.write(`<h1> <style> h1{text-align: center;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            margin: 150px;
+            color: lightskyblue} </style>Você passou para a próxima fase.</h1>
+
+        <a href="hospital.html">Continuar</a>`)
+    }
+
+    else if(opcao == 2){
+        document.write(`
+            <p> <style> p {display: flex;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+                color: red;
+                font-size: 40px;
+                justify-content: center;
+                margin-top: 20%;} </style> Escolha errada, você perdeu.</p>
+
+        <a href="/projeto-modulo1/index.html">Recomeçar</a>`)
+    }
+}
+
+escolha();
---
+++ personagem.js
@@ -0,0 +1,41 @@
+function escolha(personagem){
+    
+    personagem = prompt("Escolha o seu personagem:\n1. Eris\n2. Harry\n3. Will");
+
+    while (personagem != 1 && personagem != 2 && personagem != 3) {
+        alert("O personagem que você escolheu não existe.");
+        personagem = prompt("Escolha o seu personagem:\n1. Eris\n2. Harry\n3. Will");
+    }
+
+    if(personagem == 1){
+        document.write(`
+            <h1> <style> h1{text-align: center;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+                margin: 150px;
+                color: lightskyblue} </style>Você escolheu a Eris.</h1>
+
+            <a href="../eris/ajuda.html">Começar</a>`)
+}
+
+    else if(personagem == 2){
+        document.write(`
+            <h1> <style> h1{text-align: center;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+                margin: 150px;
+                color: lightgreen} </style>Você escolheu o Harry.</h1>
+
+            <a href="../harry/procura.html">Começar</a>`)
+    }
+
+    else if(personagem == 3){
+        document.write(`
+            <h1> <style> h1{text-align: center;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+                margin: 150px;
+                color: mediumpurple;} </style>Você escolheu o Will.</h1>
+
+            <a href="../will/vinganca.html">Começar</a>`)
+    }
+}
+
+escolha();
